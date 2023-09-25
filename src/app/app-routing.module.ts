cb54a66775726359669dde88ca6886053f023882
ERAYDIN <berkay.eraydin@etiya.com>
2023-09-25 11:06:30 +0300

    init two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product',
+    loadChildren: () =>
+    import('./features/product/product.module').then((m) => m.ProductModule),
+  },
+  {
+    path: 'order',
+    loadChildren: () =>
+    import('./features/order/order.module').then((m) => m.OrderModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared-module';
+import { CoreModule } from './core/core.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'tr',
+      useDefaultLang: true,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (httpClient: HttpClient) => {
+          return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+        },
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
