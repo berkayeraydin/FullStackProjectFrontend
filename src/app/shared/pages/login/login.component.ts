e48be1a387be66c4d531ae87bb2e9ad70197edb9
Berkay ERAYDIN <eraydinberkay@outlook.com>
2023-09-25 13:29:44 +0300

    login reactive forms

--- src/app/shared/pages/login/login.component.ts
+++ src/app/shared/pages/login/login.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  // reactive forms
+  // değişkenler-ctor-metotlar
+  loginForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.buildForm();
+  }
+  buildForm() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      rememberMe: new FormControl(false, []),
+    });
+
+  }
+  submitForm() {
+    if (this.loginForm.invalid) {
+      console.log('Login Formu Hatalı iken gönderilmeye çalışıldı.');
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
 }
--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -7,13 +7,16 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { LoginComponent } from './pages/login/login.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HomepageComponent, MainLayoutComponent, LoginComponent],
   imports: [
     CommonModule,
     SharedRoutingModule,
-    TranslateModule
+    TranslateModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [MainLayoutComponent] // dış modüllerin kullanabileceği, dışarıya export edilen moduller
 })
