d55adc564eb09321930ee5756e0a1974b5051ea8
Berkay ERAYDIN <eraydinberkay@outlook.com>
2023-09-30 17:55:56 +0300

    local

--- src/app/features/product/services/product.service.ts
+++ src/app/features/product/services/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.development';
 import { GetAllProductsModel } from '../models/getAllProductsModel';
 import { AddProductModel } from '../models/addProductModel';
 import { UpdateProductModel } from '../models/updateProductModel';
